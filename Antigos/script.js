19aa8baa640e7f1cdde68ff70130f6a0d4c3e0ae
otluiz <olto@ecomp.poli.br>
2023-11-14 15:43:59 -0300

    upload das paginas

---
+++ Antigos/script.js
@@ -0,0 +1,18 @@
+
+// Exemplo de credencial simulada
+const correctUsername = 'usuario';
+const correctPassword = 'senha123';
+
+document.getElementById('login-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    // Check se o username e password estão correct (simplificado)
+    if (username === correctUsername && password === correctPassword) {
+        // Redireciona para página de login se tiver sucesso
+        window.location.href = '/pagina-inicial';
+    } else {
+        // Redireciona para página de erro de falhar
+        window.location.href = '/pagina-error';
+    }
+});
